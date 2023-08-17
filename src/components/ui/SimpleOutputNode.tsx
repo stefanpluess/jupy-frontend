17ccda35878ccfa2e9482d7150ad5509695b9bbc
diegobugmann <diegoricardo.bugmann@uzh.ch>
2023-08-17 16:09:20 +0200

    Multi outputs (incl. grouping), copy images, display model training. close #40, close #113

--- src/components/ui/SimpleOutputNode.tsx
+++ src/components/ui/SimpleOutputNode.tsx
@@ -21,162 +21,96 @@ import { getConnectedNodeId } from "../../helpers/utils";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { OutputNodeData } from "../../config/types";
+import * as clipboard from "clipboard-polyfill";
 
 function SimpleOutputNode({ id, data }: NodeProps<{ outputs: OutputNodeData[] }>) {
   const hasParent = useStore((store) => !!store.nodeInternals.get(id)?.parentNode);
   const detachNodes = useDetachNodes();
-
-  const [output, setOutput] = useState({ __html: "" });
+  const [groupedOutputs, setGroupedOutputs] = useState([] as OutputNodeData[]);
 
   useEffect(() => {
-    // console.log(id+ " ----- Output Changed ----- now: " + data?.output)
-    var formattedOutput = "";
-    data?.outputs?.forEach((output) => {
-      if (output?.isImage) {
-        formattedOutput +=
-          '<img src="data:image/png;base64,' + output?.output + '"><br>';
-      } else {
-        formattedOutput += output?.output.replace(/\n/g, "<br>");
+    if (!data.outputs) return;
+
+    const grouped = [] as OutputNodeData[];
+    let currentGroup = null as OutputNodeData | null;
+    data.outputs.forEach((output) => {
+      if (!currentGroup || currentGroup.isImage || output.isImage) {
+        currentGroup = {
+          output: '',
+          isImage: output.isImage,
+          outputType: output.isImage ? 'display_data' : 'stream',
+          timestamp: output.timestamp,
+        };
+        grouped.push(currentGroup);
       }
+      // in case of live updates (f.ex. training a model), \b is used to delete the previous output
+      // display the output in a way that the \b is respected (removing from prevous output, replacing with '' in current output)
+      const strippedOutput = output.output.split('\b')
+      const indexOfCorrectOutput = strippedOutput.length - 1;
+      if (indexOfCorrectOutput > 0) currentGroup.output = currentGroup.output.slice(0, -indexOfCorrectOutput);
+      currentGroup.output += strippedOutput[indexOfCorrectOutput];
     });
-    const outputHtml = { __html: formattedOutput };
-    setOutput(outputHtml);
+
+    setGroupedOutputs(grouped);
   }, [data.outputs]);
 
   //   const onDelete = () => deleteElements({ nodes: [{ id }] });
   const onDetach = () => detachNodes([id]);
 
-  const copyOutput = () => {
-    // if data.outputs is empty, then there is no output to copy
-    if (data?.outputs[0]?.output === "") {
-      alert(
-        "No output to copy yet mate.\nFirst put in some work and execute the cell bro:)"
-      );
-      return;
-    }
-
-    //TODO: copy an image to clipboard (currently not working) --> see https://stackoverflow.com/questions/66649604/how-to-copy-an-image-to-the-clipboard-in-react
-
-    if (data?.outputs[0]?.isImage) {
-      // Create an image element and set its source to the base64 data
-      const img = new Image();
-      img.src = "data:image/png;base64," + data?.outputs[0]?.output;
-
-      // When the image is loaded, handle the clipboard copy
-      img.onload = () => {
-        // Create a canvas element and draw the image on it
-        const canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext("2d");
-        ctx?.drawImage(img, 0, 0);
-
-        // Convert the canvas data to a data URL
-        const dataURL = canvas.toDataURL();
-
-        // Copy the data URL to the clipboard
-        navigator.clipboard
-          .writeText(dataURL)
-          .then(() => {
-            alert("Copied Image to Clipboard!");
-          })
-          .catch((error) => {
-            console.error("Failed to copy image to clipboard:", error);
-          });
-      };
-    }
-    // Create a new anchor link
-    //   const link = document.createElement("a");
-    //   link.href = "data:image/png;base64," + data?.output;
-    //   link.download = "image.png"; // Set a default filename for the downloaded image
-    //   link.style.display = "none"; // Hide the link
-
-    //   // Add the link to the DOM
-    //   document.body.appendChild(link);
-
-    //   // Programmatically click the link to trigger the download
-    //   link.click();
-
-    //   // Remove the link from the DOM after a short delay
-    //   setTimeout(() => {
-    //     document.body.removeChild(link);
-    //   }, 1000);
-    // }
-
-    // if (data?.isImage) {
-    //   // Fetch the image data from the URL
-    //   fetch(data.output)
-    //     .then((response) => response.blob())
-    //     .then((blob) => {
-    //       // Convert the blob to a data URL
-    //       const reader = new FileReader();
-    //       reader.onloadend = () => {
-    //         if (typeof reader.result === "string") {
-    //           // Check if it's a valid string
-    //           // Copy the data URL to the clipboard
-    //           navigator.clipboard
-    //             .writeText(reader.result)
-    //             .then(() => {
-    //               alert("Copied Image Output to Clipboard!");
-    //             })
-    //             .catch((error) => {
-    //               console.error("Failed to copy image to clipboard:", error);
-    //             });
-    //         } else {
-    //           console.error("Invalid data format: expected a string.");
-    //         }
-    //       };
-    //       reader.readAsDataURL(blob);
-    //     })
-    //     .catch((error) => {
-    //       console.error("Failed to fetch image:", error);
-    //     });
-
-    //return;
-    else {
-      var copiedOutput = output.__html.replace(/<br>/g, "\n");
+  const copyOutput = async (index: number) => {
+    // return if there is no output yet
+    if (groupedOutputs[index]?.output === "") return;
+    if (!groupedOutputs[index]?.isImage) {
+      var copiedOutput = groupedOutputs[index].output;
       navigator.clipboard.writeText(copiedOutput);
       alert("Copied Output:\n" + copiedOutput);
       console.log("Copied Output:\n" + copiedOutput);
+    } else {
+      const item = new clipboard.ClipboardItem({
+        "image/png": b64toBlob(groupedOutputs[index]?.output, 'image/png', 512)
+      });
+      await clipboard.write([item]);
+      alert("Copied Image as PNG to Clipboard!");
     }
   };
 
-  const saveOutput = () => {
-    // first if clause can be deleted if saveOutput is working
-    if (data?.outputs[0]?.output === "") {
-      alert(
-        "No output to save yet mate.\nFirst put in some work and execute the cell bro:)"
-      );
-      return;
+  function b64toBlob(b64Data: string, contentType = 'image/png', sliceSize = 512) {
+    let byteCharacters = atob(b64Data)
+    let byteArrays = []
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      let slice = byteCharacters.slice(offset, offset + sliceSize)
+      let byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i)
+      }
+      var byteArray = new Uint8Array(byteNumbers)
+      byteArrays.push(byteArray)
     }
-    if (data?.outputs[0]?.isImage) {
+    return new Blob(byteArrays, {type: contentType})
+  }
+
+  const saveOutput = (index: number) => {
+    // return if there is no output yet
+    if (groupedOutputs[index]?.output === "") return;
+    if (groupedOutputs[index]?.isImage) {
       // Create a new anchor link
       const link = document.createElement("a");
-      link.href = "data:image/png;base64," + data?.outputs[0]?.output;
+      link.href = "data:image/png;base64," + groupedOutputs[index]?.output;
       link.download = "image.png"; // Set a default filename for the downloaded image
       link.style.display = "none"; // Hide the link
-
-      // Add the link to the DOM
-      document.body.appendChild(link);
-
-      // Programmatically click the link to trigger the download
-      link.click();
-
+      document.body.appendChild(link); // Add the link to the DOM
+      link.click(); // Programmatically click the link to trigger the download
       // Remove the link from the DOM after a short delay
       setTimeout(() => {
         document.body.removeChild(link);
       }, 1000);
-
-      // alert("Saved Output:\n" + link);
       // console.log("Saved Output:\n" + link);
       // alert("Saved Output:\n" + link);
     }
   };
 
   // INFO :: lock functionality - observe the lock state of the connected SimpleNode with code
-  const isSimpleNodeLocked = useNodesStore(
-    (state) => state.locks[getConnectedNodeId(id)]
-  );
+  const isSimpleNodeLocked = useNodesStore((state) => state.locks[getConnectedNodeId(id)]);
   const MySwal = withReactContent(Swal);
   const showAlertDetachOff = () => {
     // window.alert('Unlock 🔓 before detaching!');
@@ -186,6 +120,14 @@ function SimpleOutputNode({ id, data }: NodeProps<{ outputs: OutputNodeData[] }>
       icon: "error",
     });
   };
+
+  function getHtmlOutput(output: OutputNodeData) {
+    if (output.isImage) {
+      return '<img src="data:image/png;base64,' + output.output + '">';
+    } else {
+      return output.output.replace(/\n/g, "<br>");
+    }
+  }
 
   return (
     <>
@@ -217,42 +159,46 @@ function SimpleOutputNode({ id, data }: NodeProps<{ outputs: OutputNodeData[] }>
           </button>
         )}
       </NodeToolbar>
-      <div className="oinputCentered obuttonArea nodrag">
-        {data?.outputs?.length === 1 && ( <button
-          title="Copy Output"
-          className="obuttonArea oUpper"
-          onClick={copyOutput}
-        >
-          <FontAwesomeIcon className="icon" icon={faCopy} />
-        </button>)}
 
-        {(data?.outputs?.length === 1 && data?.outputs[0]?.isImage) && (
-          <button
-            className="obuttonArea oLower"
-            title="Save Output"
-            onClick={saveOutput}
-            //disabled={!data?.isImage} --> disabled and turning red if output != image
-            //disabled={!data.output} --> as long as there is no output button is disabled and red
-          >
-            <FontAwesomeIcon className="icon" icon={faSave} />
-          </button>
-        )}
+      <div style={{ maxHeight: "200px", maxWidth: "500px", overflow: "auto" }}>
+        {groupedOutputs.map((groupedOutput, index) => (
+          <>
+            <div className="oinputCentered obuttonArea nodrag">
+              <button
+                title="Copy Output"
+                className="obuttonArea oUpper"
+                onClick={() => copyOutput(index)}
+              >
+                <FontAwesomeIcon className="icon" icon={faCopy} />
+              </button>
+
+              {data.outputs[index]?.isImage && (
+                <button
+                  className="obuttonArea oLower"
+                  title="Save Output"
+                  onClick={() => saveOutput(index)}
+                >
+                  <FontAwesomeIcon className="icon" icon={faSave} />
+                </button>
+              )}
+            </div>
+            
+            <div
+              className="outputNode"
+              dangerouslySetInnerHTML={{ __html: getHtmlOutput(groupedOutput) }}
+              // style={groupedOutput.isImage ? { maxHeight: "200px", maxWidth: "500px", overflow: "auto" } : {}}
+            ></div>
+          </>
+        ))}
       </div>
-
-      {/* {data?.isImage ? ( */}
-        {/* <div
-          className="outputNode" //to be deleted???
-          dangerouslySetInnerHTML={output}
-          style={{ maxHeight: "400px", maxWidth: "500px", overflow: "auto" }}
-        ></div> */}
-      {/* ) : ( */}
+      {/* TODO: change this */}
+      {groupedOutputs.length === 0 && (
         <div
           className="outputNode"
-          dangerouslySetInnerHTML={output}
+          dangerouslySetInnerHTML={{ __html: "" }}
           style={{ maxHeight: "200px", maxWidth: "500px", overflow: "auto" }}
         ></div>
-      {/* )} */}
-
+      )}
       <Handle type="target" position={Position.Left} />
     </>
   );
