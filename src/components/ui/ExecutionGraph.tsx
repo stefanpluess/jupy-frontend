417dd365dfe05fa5b53174cf6edc0b2350279bb9
Szymon Kaczmarski <szymon.kaczmarski@hotmail.com>
2024-01-14 00:25:21 +0100

    #270, ExecutionGraph ✅, ExecutionGraphNode functionalities to be added

--- src/components/ui/ExecutionGraph.tsx
+++ src/components/ui/ExecutionGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import ReactFlow, {
   useReactFlow,
   Edge,
@@ -9,159 +9,190 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 import ExecutionGraphNode from './ExecutionGraphNode';
 import ExecutionGraphEdge from './ExecutionGraphEdge';
+import useExecutionStore from "../../helpers/executionStore";
+import { ExecInfo } from "../../config/types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faArrowsDownToLine, 
+  faArrowsUpToLine, 
+  faMagnifyingGlassMinus, 
+  faMagnifyingGlassPlus 
+} from '@fortawesome/free-solid-svg-icons';
+import Thanos_gif from '../../../public/thanos.gif'; // https://giphy.com/stickers/marvelstudios-oh-thanos-snapped-TfjfIgE9YUgdyz8V1J
 
+// constants and parameters
 const nodeTypes = { exeGraphNode: ExecutionGraphNode };
 const edgeTypes = { ExecutionGraphEdge: ExecutionGraphEdge };
-
-const defaultNodes: Node[] = [
-    {
-      id: '1',
-      data: {
-        node_id: 1,
-        execution_count: 1,
-        type: 'NormalExecution',
-        // code?
-      },
-      position: { x: 0, y: 0 },
-      type: 'exeGraphNode',
-    },
-    {
-      id: '2',
-      data: {
-        node_id: 2,
-        execution_count: 2,
-        type: 'NormalExecution',
-        // code?
-      },
-      position: { x: 0, y: 50 },
-      type: 'exeGraphNode',
-    },
-    {
-      id: '3',
-      data: {
-        node_id: 3,
-        execution_count: 3,
-        type: 'PropagateExecution',
-      },
-      position: { x: 0, y: 100 },
-      type: 'exeGraphNode',
-    },
-    {
-      id: '4',
-      data: {
-        node_id: 4,
-        execution_count: 4,
-        type: 'LoadParent',
-      },
-      position: { x: 0, y: 150 },
-      type: 'exeGraphNode',
-    },
-    {
-      id: '5',
-      data: {
-        node_id: 5,
-        execution_count: 5,
-        type: 'Export',
-      },
-      position: { x: 0, y: 200 },
-      type: 'exeGraphNode',
-    },
-    {
-      id: '6', // 
-      data: {
-        node_id: 6,
-        execution_count: 6,
-        type: 'NormalExecution',
-        // code?
-      },
-      position: { x: 0, y: 250 },
-      type: 'exeGraphNode',
-    },
-  ];
-
 const proOptions = { hideAttribution: true };
-
+const PADDING = 10;
+const MIN_ZOOM = 1;
+const MAX_ZOOM = 1.5;
 interface ExecutionGraphProps {
   id: string;
 }
   
 const ExecutionGraph = ({ id }: ExecutionGraphProps) => {
-    // const [nodes, setNodes, onNodesChange] = useNodesState(defaultNodes);
-    // const [edges, setEdges, onEdgesChange] = useEdgesState(defaultEdges);
-    // const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), []);
-    // TODO - code displaying
-    // TODO - what happens when it is open and we execute a node?
-    // TODO - define constants for type
-    // TODO - define a function to create nodes & edges from store
-    // TODO - add ZoomIn and ZoomOut buttons
-    // TODO - add treeHeight slider
-    // TODO - add Pan to Top button
+    const { zoomIn, zoomOut, fitView} = useReactFlow();
+    const historyPerNode = useExecutionStore((state) => state.historyPerNode[id]); // can be undefined
+    const { nodes: initNodes, edges: initEdges, lastY } = createNodesAndEdges(historyPerNode);
+    // execute fitView on historyPerNode change
+    useEffect(() => {
+      fitView({
+        padding: PADDING,
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        nodes: getLastNode(),
+        duration: 800
+      });
+    } , [fitView, initNodes]);
 
-    const { setViewport, zoomIn, zoomOut } = useReactFlow();
-  
-    const handleTransform = useCallback(() => {
-      setViewport({ x: 0, y: 0, zoom: 1 }, { duration: 800 });
-    }, [setViewport]);
+    const getLastNode = useCallback(() => {
+      return initNodes.length ? [{ id: initNodes[initNodes.length - 1].id }] : [];
+    }, [historyPerNode, initNodes]);
 
-
-    // create a function that returns defaultEdges where for each node in defaultNodes creates and edge to the next node
-    const createEdges = (nodes: Node[]) => {
-      let edges: Edge[] = [];
-      for (let i = 0; i < nodes.length - 1; i++) {
-        edges.push({
-          id: `${nodes[i].id}=>${nodes[i+1].id}`,
-          source: nodes[i].id,
-          target: nodes[i+1].id,
-          type: 'ExecutionGraphEdge',
-          markerEnd: {
-            type: MarkerType.ArrowClosed,
-            width: 15,
-            height: 15,
-          },
+    const jumpToBottom = useCallback(() => {
+      fitView({ 
+        padding: PADDING,
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        nodes: getLastNode(),
+        duration: 800
         });
-      }
-      return edges;
-    }
+    }, [fitView, historyPerNode, initNodes]);
 
-    const defaultEdges = createEdges(defaultNodes);
+    const jumpToTop = useCallback(() => {
+      fitView({ 
+        padding: PADDING,
+        minZoom: MIN_ZOOM,
+        maxZoom: MAX_ZOOM,
+        // go to the first node
+        nodes: initNodes.length ? [{ id: initNodes[0].id }] : [],
+        duration: 800
+        });
+    }, [fitView, historyPerNode, initNodes]);
 
     return (
         <ReactFlow
             className = "exegraph-flow"
-            // onNodesChange={onNodesChange}
-            // onEdgesChange={onEdgesChange}
-            // onConnect={onConnect}
-            nodes={defaultNodes}
-            edges={defaultEdges}
+            nodes={initNodes}
+            edges={initEdges}
             nodeTypes={nodeTypes}
             edgeTypes={edgeTypes}
+            // COMMENT :: view setting upon initial showing of the graph
+            fitView = {true}
+            fitViewOptions = {{
+              padding: PADDING,
+              minZoom: MIN_ZOOM,
+              maxZoom: MAX_ZOOM,
+              // always show the last node from initNodes
+              nodes: getLastNode()
+            }}
+            // COMMENT :: general flow settings
+            // left corner: [size to the left, size to the top], right corner: [size to the right, size to the bottom]
+            translateExtent = {[[-100, -160], [100, lastY+160]]}
+            nodeExtent = {[[-100, -160], [100, lastY+160]]}
+            minZoom={MIN_ZOOM}
+            maxZoom={MAX_ZOOM}
+            proOptions={proOptions}
             preventScrolling = {false}
             panOnScroll = {true}
-            fitView = {true}
-            fitViewOptions = {{padding: 0, minZoom: 1, maxZoom: 1.5,
-                // get the size of the group node and adjust how many nodes are shown
-                // TODO
-                // nodes: defaultNodes.slice(0, 3).map((node) => ({ id: node.id })),
-            }}
-
-            // TODO: this needs to be dependen on the amout of nodes we have
-            // left corner: [size to the left, size to the top], right corner: [size to the right, size to the bottom]
-            translateExtent = {[[-100, -10], [100, 300]]}
-            nodeExtent = {[[-100, -10], [100, 300]]}
-            proOptions={proOptions}
             nodesDraggable = {false}
             nodesConnectable={false}
             zoomOnDoubleClick={false}
             deleteKeyCode={null}
         >
-            {/* TODO: this also needs the zoom to be defined min, max */}
-            {/* <Panel position="top-right">
-              <button onClick={() => zoomIn({ duration: 800 })}>zoom in</button>
-              <button onClick={() => zoomOut({ duration: 800 })}>zoom out</button>
-              <button onClick={handleTransform}>pan to center(0,0,1)</button>
-            </Panel> */}
+            {historyPerNode && 
+              <Panel
+                className='exegraph-flow-panel'
+                position='top-right'>
+                  <button
+                    title="Jump to the top"
+                    className='exegraph-flow-button nodrag' 
+                    onClick={jumpToTop}>
+                      <FontAwesomeIcon className='exegraph-flow-button-icon' icon={faArrowsUpToLine} />
+                  </button>
+                  <button
+                    title="Zoom in"
+                    className='exegraph-flow-button nodrag' 
+                    onClick={() => zoomIn({ duration: 50 })}>
+                      <FontAwesomeIcon className='exegraph-flow-button-icon' icon={faMagnifyingGlassPlus} />
+                  </button>
+                  <button
+                    title="Zoom out"
+                    className='exegraph-flow-button nodrag' 
+                    onClick={() => zoomOut({ duration: 50 })}>
+                      <FontAwesomeIcon className='exegraph-flow-button-icon' icon={faMagnifyingGlassMinus} />
+                  </button>
+                  <button
+                    title="Jump to the bottom"
+                    className='exegraph-flow-button nodrag' 
+                    onClick={jumpToBottom}>
+                      <FontAwesomeIcon className='exegraph-flow-button-icon' icon={faArrowsDownToLine} />
+                  </button>
+              </Panel>
+            }
+            {/* display when execution graph is empty */}
+            {historyPerNode === undefined &&
+              <Panel
+                className='exegraph-flow-panel-empty'
+                position='top-center'>
+                  <div className="exegraph-flow-panel-empty-text">
+                    <img src={Thanos_gif} alt="" style={{ width: '40%', height: 'auto' }} />
+                    <p>Execution Graph is empty, execute something!</p>
+                  </div>
+              </Panel>
+            }
         </ReactFlow>
     );
+  };
+
+  const createNodes = (historyPerNode: ExecInfo[]) => {
+    if (!historyPerNode) {
+      return { nodes: [], lastY: 0 };
+    }
+  
+    let nodes: Node[] = [];
+    let y = 0;
+    for (let i = 0; i < historyPerNode.length; i++) {
+      const { node_id, execution_count, type, code } = historyPerNode[i];
+      nodes.push({
+        id: `${i}`,
+        data: {
+          node_id,
+          execution_count,
+          type,
+          code
+        },
+        position: { x: 0, y },
+        type: 'exeGraphNode',
+      });
+      y += 50;
+    }
+    return { nodes, lastY: y };
+  };
+  
+  const createEdges = (nodes: Node[]) => {
+    let edges: Edge[] = [];
+    for (let i = 0; i < nodes.length - 1; i++) {
+      edges.push({
+        id: `${nodes[i].id}=>${nodes[i+1].id}`,
+        source: nodes[i].id,
+        target: nodes[i+1].id,
+        type: 'ExecutionGraphEdge',
+        markerEnd: {
+          type: MarkerType.ArrowClosed,
+          width: 15,
+          height: 15,
+        },
+      });
+    }
+    return edges;
+  }
+
+  const createNodesAndEdges = (historyPerNode: ExecInfo[]) => {
+    const { nodes, lastY } = createNodes(historyPerNode);
+    const edges = createEdges(nodes);
+    return { nodes, edges, lastY };
   };
 
 export default ExecutionGraph;
--- src/components/ui/ExecutionGraphNode.tsx
+++ src/components/ui/ExecutionGraphNode.tsx
@@ -28,19 +28,18 @@ import {
   import withReactContent from "sweetalert2-react-content";
   //COMMENT :: Internal modules HELPERS
   //COMMENT :: Internal modules CONFIG
-  import {
-    KERNEL_BUSY_FROM_PARENT,
-  } from "../../config/constants";
+  import { ExecInfoT } from "../../config/constants";
   //COMMENT :: Internal modules UI
   //COMMENT :: Internal modules BUTTONS
   
  
   function ExecutionGraphNode({ id, data }: NodeProps) {
-    // 1. NormalExecution - hover(highlight) + click(show code) + magnifier(transport) + tooltip what happened in text upon hover
-    // 2. PropagateExecution - hover(highlight) + click(show code) + magnifier(transport) + tooltip what happened in text upon hover
-    // 3. LoadParent - hover(highlight parent/edge + tooltip what happened in text upon hover)
-    // 4. Export - hover(tooltip what happened in text upon hover)
-    // 5. LoadLibraries- hover(tooltip what happened in text upon hover)
+    // TODO - add hovering and clicking functionality
+    // 1. ExecInfoT.NormalExecution - hover(highlight) + click(show code) + magnifier(transport) + tooltip what happened in text upon hover
+    // 2. ExecInfoT.PropagateExecution - hover(highlight) + click(show code) + magnifier(transport) + tooltip what happened in text upon hover
+    // 3. ExecInfoT.LoadParent - hover(highlight parent/edge + tooltip what happened in text upon hover)
+    // 4. ExecInfoT.Export - hover(tooltip what happened in text upon hover)
+    // 5. ExecInfoT.LoadLibraries- hover(tooltip what happened in text upon hover)
     // 6. Deleted Nodes: Don't highlight any node. Indicate deletion (make nodes greyed out)
 
     return (
@@ -59,27 +58,27 @@ import {
           {/* COMMENT: node type icon */}
           <div className="exegraph-node-body">
               <div className="exegraph-node-body-icon">
-                {/* 'NormalExecution' - faCode/faPlay, 
-                'PropagateExecution' - faCode/faPlay + faArrowTurnDown, 
-                'LoadParent' - faCloudArrowDown/faDownload
-                'Export' - faCloudArrowUp/faUpload
-                'LoadLibraries' - faBook/faCubesStacked*/}
-                {data.type === 'NormalExecution' && (
+                {/* ExecInfoT.NormalExecution - faCode/faPlay, 
+                ExecInfoT.PropagateExecution - faCode/faPlay + faArrowTurnDown, 
+                ExecInfoT.LoadParent - faCloudArrowDown/faDownload
+                ExecInfoT.Export - faCloudArrowUp/faUpload
+                ExecInfoT.LoadLibraries - faBook/faCubesStacked*/}
+                {data.type === ExecInfoT.NormalExecution && (
                   <FontAwesomeIcon icon={faCode} />
                 )}
-                {data.type === 'PropagateExecution' && (
+                {data.type === ExecInfoT.PropagateExecution && (
                   <>
                     <FontAwesomeIcon icon={faCode} />
                     <FontAwesomeIcon className="exegraph-node-body-icon-small" icon={faArrowTurnDown} />
                   </>
                 )}
-                {data.type === 'LoadParent' && (
+                {data.type === ExecInfoT.LoadParent && (
                   <FontAwesomeIcon icon={faCloudArrowDown} />
                 )}
-                {data.type === 'Export' && (
+                {data.type === ExecInfoT.Export && (
                   <FontAwesomeIcon icon={faCloudArrowUp} />
                 )}
-                {data.type === 'LoadLibraries' && (
+                {data.type === ExecInfoT.LoadLibraries && (
                   <FontAwesomeIcon icon={faBook} />
                 )}
 
@@ -87,11 +86,10 @@ import {
           </div>
 
           {/* COMMENT: node top icon action */}
-          {/* Display icon only in case we have data.type == NormalExecution or PropagateExecution */}
-          {(data.type === 'NormalExecution' || data.type === 'PropagateExecution') && (
+          {/* Display icon only in case we have data.type == ExecInfoT.NormalExecution or ExecInfoT.PropagateExecution */}
+          {(data.type === ExecInfoT.NormalExecution || data.type === ExecInfoT.PropagateExecution) && (
             <div className="exegraph-node-top">
                 <FontAwesomeIcon className='exegraph-node-top-icon' icon={faMagnifyingGlass} />
-
             </div>
           )}
 
